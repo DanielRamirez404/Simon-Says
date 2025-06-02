c8878186da9f6fb515146960b881bdec4b00c1fb
DanielRamirez404 <danielramirezabou404@gmail.com>
2025-06-01 20:33:40 -0400

    Connected the LCD to the codebase

--- simon-says.ino
+++ simon-says.ino
@@ -1,4 +1,7 @@
 #include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C* lcd = nullptr;
 
 String addressToString(byte address);
 byte charToByte(char c);
@@ -7,18 +10,28 @@ String getLcdAddress();
 
 void setup() {
   Wire.begin();
-
   Serial.begin(9600);
   while (!Serial);
+
+  const String lcd_address{ getLcdAddress() };
+
+  while (lcd_address == "")
+    Serial.println("Error: LCD address not found");
+
+  const byte lcd_byteAddress{ stringToByte(lcd_address) };
+
+  lcd = new LiquidCrystal_I2C{lcd_byteAddress, 16 , 2};
+
+  lcd->init();
+  lcd->clear();         
+  lcd->backlight();
+  
+  lcd->setCursor(0,0);
+  lcd->print("Hello world!");
 }
 
 void loop() {
-  static String lcd_address{ getLcdAddress() };
 
-  if (lcd_address == "")
-    return;
-
-  Serial.println(stringToByte(lcd_address), HEX);
 }
 
 String addressToString(byte address) {
